961c9d495cd302f19a01a5b369c01dcbe3be9e89
caiseL <cristophertraperoo@gmail.com>
2025-05-23 09:40:00 -0700

    feat: add create staff endpoint

--- back/src/api/index.ts
+++ back/src/api/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import clients from './clients';
+import staff from './staff';
 
 const router = express.Router();
 
 router.use('/clients', clients);
+router.use('/staff', staff);
 
 export default router;
